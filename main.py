9b88c97064b0d4c59d9ae2a491c446b1e91b4ba7
fafatera87 <162295048+fafatera87@users.noreply.github.com>
2024-05-01 16:59:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 import datetime
 from pyrogram import Client
+from config import API_ID, API_HASH
 
 # Function to check bot's activity status
 def check_bot_status(user_client, bot_username):
@@ -41,8 +42,8 @@ def main():
         bot_usernames_str = os.environ.get("ArabUltraUbot ArabV2Ubot supernovaxubot DayforuMusic_bot ArabxRobot AfterGankUbot SASProtectV1_Bot SonixUbot OnedayXUbot RoyalUbot MydamnUbot fsubprem_1bot DomiUbot")
         update_channel = os.environ.get("-1001837260549")
         status_message_ids_str = os.environ.get("43")
-        api_id = int(os.environ.get("29737623"))
-        api_hash = os.environ.get("71a4bb6501593f225cdab4d4b368a830")
+        api_id=API_ID,
+        api_hash=API_HASH,
 
         # Inisialisasi klien Pyrogram dengan kunci API
         user_client = Client(session_name=str(user_session_string), api_id=api_id, api_hash=api_hash)
